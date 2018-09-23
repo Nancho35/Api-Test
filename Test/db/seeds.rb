4c65a4be8443a7fcf85066024ce0fcda82ac62a2
Hernan <hernandario35@gmail.com>
2018-09-23 15:58:19 -0500

    Cargue inicial

--- Test/db/seeds.rb
+++ Test/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+json = ActiveSupport::JSON.decode(File.read('db/seeds/preguntas.json'))
+
+json.each do |a|
+  Country.create!(a['pregunta'], without_protection: true)
+end
